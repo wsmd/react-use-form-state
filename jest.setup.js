94ffb2a21ee3be73dfcf409e117201e473b49ead
Waseem Dahman <dwaseem@icloud.com>
2018-11-05 22:48:36 -0500

    Refactor tests and use React for rendering

---
+++ jest.setup.js
@@ -0,0 +1,19 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+import spyMatchers from 'expect/build/spy_matchers';
+
+import 'jest-dom/extend-expect';
+import 'react-testing-library/cleanup-after-each';
+
+expect.extend({
+  /**
+   * Test if a mock function was called with an object containing a `values` key
+   * with a value matching `valuesMatching`
+   */
+  toHaveBeenLastCalledWithValues(changeHandler, valuesMatching) {
+    const valuesMatcher = expect.objectContaining({
+      values: valuesMatching,
+    });
+    return spyMatchers.toHaveBeenLastCalledWith(changeHandler, valuesMatcher);
+  },
+});
---
+++ test/test-utils.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import { useFormState } from '../src';
+
+const noop = () => {};
+
+const InputForm = ({ onChange, name, value, type }) => {
+  const [formState, input] = useFormState();
+  onChange(formState);
+  return <input {...input[type](name, value)} required />;
+};
+
+InputForm.defaultProps = {
+  onChange: noop,
+};
+
+export function renderInput(type, name, value) {
+  const onChangeMock = jest.fn();
+  const { container } = render(
+    <InputForm type={type} name={name} value={value} onChange={onChangeMock} />,
+  );
+  const input = container.firstChild;
+  return {
+    input,
+    changeHandler: onChangeMock,
+    blur: () => fireEvent.blur(input),
+    change: target => fireEvent.change(input, { target }),
+    click: () => fireEvent.click(input),
+  };
+}
+
+export function mockReactUseReducer() {
+  let spy;
+  const useReducerMock = (reducer, initialState = {}) => [initialState, noop];
+  beforeAll(() => {
+    spy = jest.spyOn(React, 'useReducer').mockImplementation(useReducerMock);
+  });
+  afterAll(() => {
+    if (spy.mockRestore) spy.mockRestore();
+  });
+}
