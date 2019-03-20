1374f1c3c47bba6d64fb3f11072e02a7c646a0d5
Waseem Dahman <dwaseem@icloud.com>
2019-03-19 20:10:04 -0400

    rework tests

---
+++ test/useFormState-formOptions.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { renderWithHook } from './test-utils';
+
+describe('useFormState options', () => {
+  it('calls options.onChange when an input changes', async () => {
+    const changeHandler = jest.fn();
+    const { change } = renderWithHook(
+      ([, { text }]) => <input {...text('username')} />,
+      null,
+      { onChange: changeHandler },
+    );
+    change({ value: 'w' });
+    expect(changeHandler).toHaveBeenCalledWith(
+      expect.any(Object), // SyntheticEvent
+      expect.objectContaining({ username: '' }),
+      expect.objectContaining({ username: 'w' }),
+    );
+  });
+
+  it('calls options.onBlur when an input changes', () => {
+    const blurHandler = jest.fn();
+    const { blur } = renderWithHook(
+      ([, { text }]) => <input {...text('username')} />,
+      null,
+      { onBlur: blurHandler },
+    );
+    blur();
+    expect(blurHandler).toHaveBeenCalledWith(expect.any(Object));
+    blur();
+    expect(blurHandler).toHaveBeenCalledTimes(2);
+  });
+
+  it('calls options.onTouched when an input changes', () => {
+    const touchedHandler = jest.fn();
+    const { blur } = renderWithHook(
+      ([, { text }]) => <input {...text('username')} />,
+      null,
+      { onTouched: touchedHandler },
+    );
+    blur();
+    expect(touchedHandler).toHaveBeenCalled();
+    blur();
+    expect(touchedHandler).toHaveBeenCalledTimes(1);
+  });
+});
