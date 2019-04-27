ef54fc212acbb205461f6293bda0e4a51f48ddb6
Waseem Dahman <dwaseem@icloud.com>
2019-04-27 09:39:54 -0400

    update tests

--- jest.setup.js
+++ jest.setup.js
@@ -2,3 +2,18 @@
 
 import 'jest-dom/extend-expect';
 import 'react-testing-library/cleanup-after-each';
+
+/**
+ * Mocking calls to console.warn to test against warnings and errors logged
+ * in the development environment.
+ */
+let consoleSpy;
+const originalEnv = process.env.NODE_ENV;
+beforeEach(() => {
+  consoleSpy = jest.spyOn(console, 'warn').mockImplementation(() => {});
+  process.env.NODE_ENV = 'development';
+});
+afterEach(() => {
+  consoleSpy.mockRestore();
+  process.env.NODE_ENV = originalEnv;
+});
