7fff37d9a624510eaaae8272c7afc5dcb7eded3a
Derrick Beining <derrick.beining@gmail.com>
2019-05-11 22:02:13 -0400

    Fix stale closures of callback options in inputs.text et. al.
    The implementation of useFormState is problematic because it
    caches callback functions passed to inputs.text et. al. This
    causes functions like onChange, validate, and onBlur to run with
    stale closures, namely, the closure with which they are
    initialised.
    
    This commit fixes the issue by removing the `callback` cache from
    the implementation. Tests that depended on this buggy behavior
    were removed as they are unsupportable features given the current
    implementation.

--- src/useCache.js
+++ src/useCache.js
@@ -5,8 +5,6 @@ export function useCache() {
   const has = key => cache.current.has(key);
   const get = key => cache.current.get(key);
   const set = (key, value) => cache.current.set(key, value);
-  const getOrSet = (key, value) =>
-    has(key) ? get(key) : set(key, value) && get(key);
 
-  return { getOrSet, set, has, get };
+  return { set, has, get };
 }
