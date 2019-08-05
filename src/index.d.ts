881148be4c53b01a70b2ba2be697a7bb433acc57
Waseem Dahman <dwaseem@icloud.com>
2019-08-05 14:38:28 -0400

    0.11.0

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for react-use-form-state 0.10.4
+// Type definitions for react-use-form-state 0.11.0
 // Project: https://github.com/wsmd/react-use-form-state
 // Definitions by: Waseem Dahman <https://github.com/wsmd>
 
